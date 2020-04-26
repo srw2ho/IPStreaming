6b7ecee34239eca1faa3b3321dd50a6a1ba87bd4
Willi Schneider <wilschneider@kabelmail.de>
2020-04-26 10:27:29 +0200

    forget to commit

--- IPStreamingCPP/FFmpegInteropExtRT/pch.h
+++ IPStreamingCPP/FFmpegInteropExtRT/pch.h
@@ -155,6 +155,15 @@ inline std::string PlatFormStringtoStdString(Platform::String^ stringRT) {
 	return stringUtf8;
 
 }
+inline Platform::String^ stringToPlatformString(std::string inputString) {
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+	std::wstring intermediateForm = converter.from_bytes(inputString);
+	Platform::String^ retVal = ref new Platform::String(intermediateForm.c_str());
+
+	return retVal;
+}
+
+
 /*
 Platform::String ^ toSPlatformSring(const char* fmt, ...)
 {
